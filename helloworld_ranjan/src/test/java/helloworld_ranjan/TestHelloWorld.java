ad24a1282cc351188cf4e74228c8b4578ab7fbbb
Prashant <prashanthranjan@gmail.com>
2021-07-22 16:03:54 +0530

    Initial push

---
+++ helloworld_ranjan/src/test/java/helloworld_ranjan/TestHelloWorld.java
@@ -0,0 +1,11 @@
+package helloworld_ranjan;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	@Test
+	public void testHelloWorld() {
+		System.out.println("hello Prashant Ranjan ...!");
+	}
+
+}
